ac716a10afd75c9c9c6d6a48422b7f1fead59c13
seveneditor <sabori780@gmail.com>
2017-07-13 14:51:45 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {212281884},--Sudo users
+    sudo_users = {419474218},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V3
